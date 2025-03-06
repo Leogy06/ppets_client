e31dc936595001888a596d702de059a51ecb8557
Leogy <2022_cete_santillanlb@online.htcgsc.edu.ph>
2025-03-06 13:09:54 +0800

    remove ! in greeting

--- src/utils/greeting.ts
+++ src/utils/greeting.ts
@@ -1,9 +1,9 @@
 export const getGreeting = () => {
   const hour = new Date().getHours();
 
-  if (hour >= 5 && hour <= 12) return "Good Morning!";
+  if (hour >= 5 && hour <= 12) return "Good Morning";
 
-  if (hour >= 12 && hour <= 18) return "Good Afternoon!";
+  if (hour >= 12 && hour <= 18) return "Good Afternoon";
 
-  return "Good Evening!";
+  return "Good Evening";
 };
