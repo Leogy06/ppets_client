053f05d65b7fc3de47d0e18b3d68660147eb0243
Leogy <2022_cete_santillanlb@online.htcgsc.edu.ph>
2025-03-07 18:45:01 +0800

    auth context error

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -48,6 +48,8 @@ export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
     } catch (error: unknown) {
       if (typeof error === "object" && error !== null && "data" in error) {
         const err = error as { data?: { message?: string } };
+        console.error("Unable to login. ", error);
+
         openSnackbar(err.data?.message || "Unable to login", "error");
       } else {
         openSnackbar("Unable to login", "error");
