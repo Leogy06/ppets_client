4430e8e3bfc56321cdbc024a640a2a5e51857171
Leogy <2022_cete_santillanlb@online.htcgsc.edu.ph>
2025-03-12 16:49:24 +0800

    added count dashboard

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,7 +1,17 @@
 import React from "react";
+import PageHeader from "../(component)/pageheader";
+import { Dashboard } from "@mui/icons-material";
 
 const AdminDashboard = () => {
-  return <div>AdminDashboard</div>;
+  return (
+    <>
+      <PageHeader icon={Dashboard} pageHead="Dashboard" />
+      <div>
+        <p>Total Request: </p>
+        <p>Today's Request</p>
+      </div>
+    </>
+  );
 };
 
 export default AdminDashboard;
