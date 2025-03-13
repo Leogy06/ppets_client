e6529193014aa4a34f92d946fbba649df6dda9c3
Leogy06 <2022_cete_santillanlb@online.htcgsc.edu.ph>
2025-03-13 22:18:51 +0800

    fix delete modal

--- src/app/admin/employee/page.tsx
+++ src/app/admin/employee/page.tsx
@@ -5,7 +5,7 @@ import {
   useGetEmployeesQuery,
 } from "@/features/api/apiSlice";
 import { dateFormmater } from "@/utils/date_formmater";
-import { Button, Modal } from "@mui/material";
+import { Button } from "@mui/material";
 import { DataGrid, GridColDef, GridRowSelectionModel } from "@mui/x-data-grid";
 import React, { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
@@ -31,47 +31,41 @@ const DeleteConfirmModal: React.FC<DeleteConfirmModalProps> = ({
   deleteEmployees,
 }) => {
   return (
-    <Modal open={open} onClose={onClose}>
-      <div className=" absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-        <div className="w-[300px] bg-white rounded-2xl flex flex-col items-center justify-center gap-5 p-7 shadow-lg relative">
-          <div className="w-full flex flex-col gap-1">
-            <p className="text-lg font-bold text-gray-900">
-              Delete Employee(s)?
-            </p>
-            <p className="text-gray-500 font-light">
-              Are you sure you want to delete the selected employee(s)? This
-              action cannot be undone.
-            </p>
-          </div>
-          <div className="w-full flex items-center justify-center gap-2">
-            <button
-              onClick={onClose}
-              className="w-1/2 h-9 rounded-lg bg-gray-300 hover:bg-gray-400 font-semibold"
-            >
-              Cancel
-            </button>
-            <button
-              onClick={deleteEmployees}
-              className="w-1/2 h-9 rounded-lg bg-red-400 text-white hover:bg-red-500 font-semibold"
-            >
-              Delete
-            </button>
-          </div>
-          <button
-            className="absolute top-5 right-5 flex items-center justify-center bg-transparent hover:text-black"
-            onClick={onClose}
-          >
-            <svg
-              height="20px"
-              viewBox="0 0 384 512"
-              className="fill-gray-400 hover:fill-black"
-            >
-              <path d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z" />
-            </svg>
-          </button>
-        </div>
+    <DefaultModal open={open} onClose={onClose}>
+      <div className="w-full flex flex-col gap-1">
+        <p className="text-lg font-bold">Delete Employee(s)?</p>
+        <p className="text-gray-500 font-light">
+          Are you sure you want to delete the selected employee(s)? This action
+          cannot be undone.
+        </p>
       </div>
-    </Modal>
+      <div className="w-full flex items-center justify-center gap-2">
+        <button
+          onClick={onClose}
+          className="w-1/2 h-9 rounded-lg bg-gray-300 hover:bg-gray-400 font-semibold"
+        >
+          Cancel
+        </button>
+        <button
+          onClick={deleteEmployees}
+          className="w-1/2 h-9 rounded-lg bg-red-400 text-white hover:bg-red-500 font-semibold"
+        >
+          Delete
+        </button>
+      </div>
+      <button
+        className="absolute top-5 right-5 flex items-center justify-center bg-transparent hover:text-black"
+        onClick={onClose}
+      >
+        <svg
+          height="20px"
+          viewBox="0 0 384 512"
+          className="fill-gray-400 hover:fill-black"
+        >
+          <path d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z" />
+        </svg>
+      </button>
+    </DefaultModal>
   );
 };
 
