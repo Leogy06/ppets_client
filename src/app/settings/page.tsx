f6ae9a28adfefcec058eeb98c0cc51f119b60ddd
Leogy06 <2022_cete_santillanlb@online.htcgsc.edu.ph>
2025-03-14 21:04:04 +0800

    added setting for the future

---
+++ src/app/settings/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+//this setting is for user interface configuration such as:
+//setting their preferred table height
+const AdtsSetting = () => {
+  return <div>AdtsSetting</div>;
+};
+
+export default AdtsSetting;
