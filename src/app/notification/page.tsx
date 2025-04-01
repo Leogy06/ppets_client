1f102141324e7cc6036d55592ee0c791c3a14078
Leogy06 <2022_cete_santillanlb@online.htcgsc.edu.ph>
2025-04-01 20:58:52 +0800

    added notification service

--- src/app/notification/page.tsx
+++ src/app/notification/page.tsx
@@ -2,14 +2,20 @@
 
 import PageHeader from "@/app/(component)/pageheader";
 import { ArrowBack, NotificationsActiveOutlined } from "@mui/icons-material";
+import { Paper } from "@mui/material";
 import { useRouter } from "next/navigation";
 import React from "react";
 
 const Notifications = () => {
   const router = useRouter();
+
+  //notification row
+  const Rows = () => (
+    <div className="w-full text-center border-b px-4 py-2">notificaiton</div>
+  );
   return (
     <>
-      <div className="flex gap-2 items-center">
+      <div className="flex gap-2 items-center mb-4">
         <button onClick={() => router.back()}>
           <ArrowBack />
         </button>
@@ -19,6 +25,11 @@ const Notifications = () => {
           hasMarginBottom={false}
         />
       </div>
+      <Paper sx={{ maxHeight: "70vh", overflow: "auto" }}>
+        <div className="flex flex-col">
+          <Rows />
+        </div>
+      </Paper>
     </>
   );
 };
