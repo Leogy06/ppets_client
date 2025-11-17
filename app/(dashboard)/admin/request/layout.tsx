ce144a77abf468b4ab4caab9080d2ae8ab694eca
Leogy <2022_cete_santillanlb@online.htcgsc.edu.ph>
2025-11-17 17:12:32 +0800

    added padding

--- app/(dashboard)/admin/request/layout.tsx
+++ app/(dashboard)/admin/request/layout.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const RequestLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <main className="container mx-auto py-10 px-8 md:px-0 space-y-4">
+    <main className="container mx-auto py-10 px-4 md:px-6 lg:px-8 space-y-4">
       {children}
     </main>
   );
